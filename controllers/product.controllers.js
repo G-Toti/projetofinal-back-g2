105a54ce7e0786a05386cc6c3b2a728694999769
Gabriel Toti <d2023005594@unifei.edu.br>
2024-04-17 17:48:11 -0300

    Adicionando rota para criar produtos.

---
+++ controllers/product.controllers.js
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      price: parseFloat(req.body.price),
+      description: req.body.description,
+      image: req.file.path,
+      stock: parseInt(req.body.stock),
+      sold: parseInt(req.body.sold),
+      cart: {
+        create: [],
+      },
+    },
+  });
+
+  res.json({
+    data: product,
+    msg: "Produto criado com sucesso!",
+  });
+};
